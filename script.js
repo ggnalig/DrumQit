737384a2d09caa8545cc1201c582cf3a4ad60cbc
unknown <rahmatgilangmaulana9@gmail.com>
2020-04-26 05:33:29 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+window.addEventListener('keydown', function(e){
+    let audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`)
+    let key = document.querySelector(`.key[data-key = "${e.keyCode}"]`)
+    if (!audio){
+      return
+    }
+    audio.currentTime = 0
+    audio.play()
+    key.classList.add('playing')
+  })
+
+  let removeTransition = function(e){
+    if (e.propertyName !== 'transform')
+    return
+    this.classList.remove('playing')
+  }
+  
+  let keys = document.querySelectorAll('.key')
+  console.log(keys)
+  keys.forEach(function(key){
+    key.addEventListener('transitionend', removeTransition)
+  })
